5677003b134224e95aa47e16d61a9dba201eb441
Aswin Antony <aswinajraj@gmail.com>
2020-07-02 22:31:15 +0530

    layout corrected and comments added in application.py

--- application.py
+++ application.py
@@ -1,13 +1,18 @@
+#for interacting with Operating Systems
 import os
 
 from flask import Flask, render_template, flash, session, request, redirect #jsonify, 
-
-from flask import Flask, session
 from flask_session import Session
+
+#for sqlalchemy connections and session management
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+#For password salting, hashing and unhashing
 from werkzeug.security import check_password_hash, generate_password_hash
+
+# Allows to send HTTP requests using Python.
+import requests
 
 app = Flask(__name__)
 #app.config['DATABASE_URL'] = "postgres://ukpdswnmbmmcuz:23b8e24df67f99e4c192428081408be924dc1745162bc32bca778db643c70e90@ec2-3-234-109-123.compute-1.amazonaws.com:5432/d6hgmt6gs7ijvf"
@@ -28,23 +33,18 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
-#@login_required
+@login_required #https://flask-login.readthedocs.io/en/latest/#flask_login.login_required
 def home():
     return render_template("home.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    #flash("This is a flashed message.")
-    #flash("Test line 2 flashed message.")
 
     # Forget any user_id
     session.clear()
 
-    # username field is rendered with an empty string as value.
+    # variable username created for being used in the username select query below
     username = request.form.get("username")
-
-    #testing password
-    password = request.form.get("password")
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
@@ -56,10 +56,29 @@ def login():
         # Ensure password was submitted
         elif not request.form.get("password"):
             return render_template("error.html", message="must provide password")
-    
-    #for testing successful login
-        if username == "admin" and password == "password":
-            return redirect("/")
+
+        # Query database for username (http://zetcode.com/db/sqlalchemy/rawsql/)
+        # https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.ResultProxy
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                            {"username": username})
+        
+        result = rows.fetchone()
+
+        """check_password_hash (pwhash, password) -- checks a password against a given salted and hashed password value 
+        :param pwhash: a hashed string like returned by generate_password_hash()
+        :param password: the plaintext password to compare against the hash."""
+
+        # Ensure username exists (if the username select query is not None) 
+        # Check password is correct by using 'check_password_hash()'
+        if result == None or not check_password_hash(result[2], request.form.get("password")):
+            return render_template("error.html", message="invalid username and/or password")
+
+        # Remember which user has logged in (columns 0 & 1 from the rows fetched using the above select query)
+        session["user_id"] = result[0]
+        session["user_name"] = result[1]
+
+        # Redirect user to home page
+        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -127,13 +146,11 @@ def register():
     else:
         return render_template("register.html")
 
-@app.route("/error")
-def error():
-    return render_template("error.html", message='Sample Error')
+
 
 # For DB connection using Terminal execute below.
 # export DATABASE_URL="postgres://ukpdswnmbmmcuz:23b8e24df67f99e4c192428081408be924dc1745162bc32bca778db643c70e90@ec2-3-234-109-123.compute-1.amazonaws.com:5432/d6hgmt6gs7ijvf"
 # export FLASK_DEBUG=1
 # export FLASK_APP=application.py
 # flask run
-# Sample user credentials : herokutest / admin123+++
+# Sample user credentials : herokutest & herokutest1 / admin123+++
