93cfdbfc475e935851d50ee662cfdb96e2c91954
Aswin Antony <63504063+aswinantony@users.noreply.github.com>
2020-07-08 08:41:04 +0530

    Update login_decorator.py
    Code Review code

--- login_decorator.py
+++ login_decorator.py
@@ -3,7 +3,7 @@ from flask import redirect, render_template, request, session
 
 def login_required(f):
     """
-    Login Required Decorator
+    Login Required Decorator - This will make sure the user is logged in before continuing if used correctly in the required route.
 
     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     
